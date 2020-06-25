bc081f5e4a57b8ee0b5a9891b972ae789ea27799
alvarohenriquecz <alvarohenriquecz@gmail.com>
2020-06-25 01:47:59 -0300

    Chapter 01, Looping

---
+++ Chapter 01/Looping/main.cpp
@@ -0,0 +1,21 @@
+// Loping.cbp
+#include <iostream>
+
+using namespace std;
+
+void Looping(int n)
+{
+    int i = 0;
+
+    while(i < n)
+    {
+        cout << i << endl;
+        i = i + 1;
+    }
+}
+
+int main()
+{
+    Looping(100);
+    return 0;
+}
