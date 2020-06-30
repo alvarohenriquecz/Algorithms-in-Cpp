17378bf9a38cb42f96c46cf85baff335dd9aa358
alvarohenriquecz <alvarohenriquecz@gmail.com>
2020-06-30 16:29:20 -0300

    Chapter 07, Binary Heap

---
+++ Chapter 07/Binary_Heap/src/BinaryHeap.cpp
@@ -0,0 +1,105 @@
+// Project: Binary_Heap.cpp
+// File   : Binary_Heap.cpp
+
+#include "BinaryHeap.h"
+
+BinaryHeap::BinaryHeap()
+{
+    // Clear the vector 
+    vect.clear();
+
+    // Root will be index 1
+    // so we set index 0 to be 'dummy
+    vect.push_back(-1);
+
+    // An empty Binary heap
+    // contains no element
+    heapSize = 0;
+}
+
+bool BinaryHeap::IsEmpty()
+{
+    // Heap is empty if only
+    // heapSize = 0
+    return heapSize == 0;
+}
+
+void BinaryHeap::ShiftUp(int index)
+{
+    // Do nothing in root
+    if(index == 1)
+        return;
+    
+    if(vect[index] > vect[p(index)])
+    {
+        // Swap Upwards
+        std::swap(vect[index], vect[p(index)]);
+
+        // Recurse until root
+        ShiftUp(p(index));
+    }
+}
+
+void BinaryHeap::Insert(int key)
+{
+    // Add a new element in vector
+    if(heapSize + 1 >= (int)vect.size())
+        vect.push_back(0);
+    
+    // Store the key in the vector last position
+    vect[heapSize++] = key;
+
+    // Shift upwards
+    ShiftUp(heapSize);
+}
+
+void BinaryHeap::ShiftDown(int i)
+{
+    // For non-exist index
+    // just do nothing
+    if(i > heapSize)
+        return;
+    
+    // Initialize swapId
+    int swapId = i;
+
+    // Compare with left child, if exist
+    if(l(i) <= heapSize && vect[i] < vect[l(i)])
+        swapId = l(i);
+    
+    // Compare with right child, if exist
+    if(r(i) <= heapSize && vect[swapId] < vect[r(i)])
+        swapId = r(i);
+    
+    // Swap with the larger of the two children
+    if(swapId != i)
+    {
+        // Swap downwards with the larger
+        // of the two children
+        std::swap(vect[i], vect[swapId]);
+
+        // Recursive until the index
+        ShiftUp(swapId);
+    }
+}
+
+int BinaryHeap::ExtractMax()
+{
+    // Maximum value is in root
+    int maxval = vect[1];
+
+    // Swap with the last existing leaf
+    std::swap(vect[1], vect[heapSize--]);
+
+    // Fix heap property downwards
+    ShiftDown(1);
+
+    // Return the maximum value
+    return maxval;
+}
+
+int BinaryHeap::GetMax()
+{
+    // Return root's key
+    return vect[1];
+}
